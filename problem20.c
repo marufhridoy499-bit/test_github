8e4bad8e2464152884e40832dbaab727c00151d6
marufhridoy499-bit <marufhridoy499@gmail.com>
2026-04-23 12:17:32 -0700

    Create problem20.c

---
+++ problem20.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+    
+    printf("The number is(1 to 10):\n");
+    while(i <= 10) {
+        printf("%d ", i);
+        i++;
+    }
+    printf("\n");
+    
+    return 0;
+}
