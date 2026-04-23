7f601035c228b0a877a55fae0df1d255299c991f
marufhridoy499-bit <marufhridoy499@gmail.com>
2026-04-23 12:15:20 -0700

    Create problem19.c

---
+++ problem19.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    
+    printf("The number (1 to 10): \n");
+    for(i = 1; i <= 10; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+    
+    return 0;
+}
